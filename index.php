c43e2796a4dc07f3efddbcc9d8fcd851f433b670
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-12-07 22:42:11 +0200

    Products FE

--- index.php
+++ index.php
@@ -3,6 +3,17 @@ include 'core/init.php';
 include 'includes/header.php';
 ?>
 
-
+ <div class="product-grid-container">
+        <?php 
+         $products = [1,2,3,4,5,6,7,8,9,10];
+        foreach ($products as $product): ?>
+            <div class="product-grid-item">
+                <img src="https://w7.pngwing.com/pngs/853/610/png-transparent-christmas-and-holiday-season-merry-christmas-food-holidays-decor.png" alt="">
+                <h5>H5 Heading</h5>
+                <p>H6 Description</p>
+                 <button class="buy-button">Buy Now</button>
+            </div>
+        <?php endforeach; ?>
+    </div>
 
 <?php include 'includes/footer.php'; ?>
