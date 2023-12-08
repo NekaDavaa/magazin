5873b444c7ecc6eb4112fa98c723e4323751a073
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-12-08 12:40:55 +0200

    Product class and xml integration

--- classes/Product.php
+++ classes/Product.php
@@ -1,8 +1,21 @@
 <?php
 class Product {
-	private $name;
-	private $price;
-   
+    
+    public function __construct() {
+
+    }
+
+    public function loadFromXml($xmlPath) {
+    	 $this->xml = simplexml_load_file("$xmlPath");
+    }
+
+    public function getAllProducts() {
+    	 $this->products = [];
+			foreach ($this->xml->record as $product) {
+    			$this->products[] = $product;
+				}
+				return $this->products;
+    }
 
 
 }
--- index.php
+++ index.php
@@ -3,17 +3,23 @@ include 'core/init.php';
 include 'includes/header.php';
 //User object
 $user = new User();
+//Product object
+$prod = new Product();
+$prod->loadFromXml("magazin_bg_products.xml");
 ?>
 
  <div class="product-grid-container">
-        <?php 
-         $products = [1,2,3,4,5,6,7,8,9,10];
-        foreach ($products as $product): ?>
+        <?php  
+          // echo "<pre>";
+          // var_dump($prod->getAllProducts());
+          // echo "</pre>";
+
+        foreach ($prod->getAllProducts() as $product): ?>
             <div class="product-grid-item">
                 <img src="https://w7.pngwing.com/pngs/853/610/png-transparent-christmas-and-holiday-season-merry-christmas-food-holidays-decor.png" alt="">
-                <h3>Product name</h3>
+                <h3><?php echo $product->product_title; ?></h3>
                  <?php if ($user->isLogged()) : ?>
-                 <p>5.00lv.</p>
+                 <p class="product-price"><?php echo $product->product_price; ?> lv.</p>
                  <button class="buy-button">Buy Now</button>
              <?php else : ?>
              	<p class="text-danger">Please log in to see prices</p>
